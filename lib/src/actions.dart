a4704b6cbfb58010489d86b7af60dcb3e49b98ac
EhisEA <37916226+EhisEA@users.noreply.github.com>
2025-01-28 12:24:04 +0100

    Update actions.dart

--- lib/src/actions.dart
+++ lib/src/actions.dart
@@ -99,8 +99,8 @@ class CustomSlidableAction extends StatelessWidget {
           style: OutlinedButton.styleFrom(
             padding: padding,
             backgroundColor: backgroundColor,
-            primary: effectiveForegroundColor,
-            onSurface: effectiveForegroundColor,
+            foregroundColor: effectiveForegroundColor,
+            //onSurface: effectiveForegroundColor,
             shape: RoundedRectangleBorder(
               borderRadius: borderRadius,
             ),
